73ddfbe7ad1efe3fbf1cec014bc1f2702ca9a072
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-18 05:49:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,6 @@
-def square_array(array)
-  new_square[]
-  count = 0 
-  while count < array.size do 
-    square_array << (array[count] ** 2)
-    count = count + 1 
-  end 
-  return squared_array
+ef square_array(numbers)
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
 end
